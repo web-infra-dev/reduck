76686f165a7119f783cc0e7d9f3f467e0beb8ac1
anc95 <1481988258@qq.com>
2021-10-13 19:47:51 +0800

    chore: thunk effect

--- examples/modern-js/effects/src/App.tsx
+++ examples/modern-js/effects/src/App.tsx
@@ -1,52 +1,30 @@
-import { Switch, Route } from '@modern-js/runtime/router';
+import { useEffect } from 'react';
+import { useModel } from "@modern-js/runtime/model";
+import todoModel from './model/todo';
 
-import './App.css';
+function Todo() {
+  const [state, actions] = useModel(todoModel);
 
-const App = () => (
-  <Switch>
-    <Route exact={true} path="/">
-      <div className="container">
-        <main>
-          <div className="logo">
-            <img
-              src="https://lf3-static.bytednsdoc.com/obj/eden-cn/ylaelkeh7nuhfnuhf/modernjs-cover.png"
-              width="300"
-              alt="Modern.js Logo"
-            />
-          </div>
-          <p className="description">
-            Get started by editing <code className="code">src/App.tsx</code>
-          </p>
-          <div className="grid">
-            <a href="#" className="card">
-              <h2>Quick Start</h2>
-            </a>
-            <a href="#" className="card">
-              <h2>Handbook</h2>
-            </a>
-            <a href="#" className="card">
-              <h2>API Reference </h2>
-            </a>
-            <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="card">
-              <h2>Community </h2>
-            </a>
-          </div>
-        </main>
-        <footer className="footer">
-          <a href="#" target="_blank" rel="noopener noreferrer">
-            Powered by Modern.JS
-          </a>
-        </footer>
+  useEffect(() => {
+    actions.load();
+    // actions.loadWithThunk();
+  }, []);
+
+  if (state.loading) {
+    return <div>loading....</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        {state.items.map((item) => (
+          <div>item</div>
+        ))}
       </div>
-    </Route>
-    <Route path="*">
-      <div>404</div>
-    </Route>
-  </Switch>
-);
+    </div>
+  );
+}
 
-export default App;
+export default function App() {
+  return <Todo />
+}
