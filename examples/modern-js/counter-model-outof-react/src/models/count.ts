781f8981edde07d0348b3f611e7bbfa61a736649
anc95 <1481988258@qq.com>
2021-10-13 17:59:08 +0800

    feat: init

---
+++ examples/modern-js/counter-model-outof-react/src/models/count.ts
@@ -0,0 +1,17 @@
+import { model } from '@modern-js/runtime/model';
+
+const countModel = model("count").define({
+  state: {
+    value: 1
+  },
+  actions: {
+    add(state) {
+      return {
+        ...state,
+        value: state.value + 1
+      };
+    }
+  }
+});
+
+export default countModel;
