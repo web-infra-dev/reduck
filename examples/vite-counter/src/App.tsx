89b4e7dda84ab608c64f4f45fd01c4104b882905
阿豪 <yanyuhao.joy@bytedance.com>
2022-06-14 14:43:37 +0800

    feat: add example (#43)

---
+++ examples/vite-counter/src/App.tsx
@@ -0,0 +1,16 @@
+import { useModel } from "@modern-js-reduck/react";
+import { modelA, modelB } from "./model/count";
+function App() {
+  const [state, actions] = useModel([modelA, modelB]);
+
+  return (
+    <div>
+      <h3>a: {state.a}</h3>
+      <h3>b: {state.b}</h3>
+      <button onClick={actions.incA}>incA</button>
+      <button onClick={actions.incB}>incB</button>
+    </div>
+  );
+}
+
+export default App;
