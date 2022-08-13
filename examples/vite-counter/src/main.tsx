7225c5c18453ab9bf2a74b69a1a701bea7cd7cd9
jason.xu <jason.xu@bytedance.com>
2022-08-13 15:58:58 +0800

    chore: modify examples

--- examples/vite-counter/src/main.tsx
+++ examples/vite-counter/src/main.tsx
@@ -3,16 +3,6 @@ import {createRoot} from "react-dom/client";
 import App from "./App";
 import { Provider } from "@modern-js-reduck/react";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById("root")!
-// );
-
-
 createRoot(document.getElementById("root")!).render(
   (<Provider>
     <App />
