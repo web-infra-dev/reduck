0fd51735830e2d87466a3c5ad14ec10a85b091ba
jason.xu <jason.xu@bytedance.com>
2023-05-11 14:09:39 +0800

    fix: fix initial state missing

--- packages/store/src/store/createStore.ts
+++ packages/store/src/store/createStore.ts
@@ -2,8 +2,10 @@ import {
   applyMiddleware,
   compose,
   createStore as createReduxStore,
-  Reducer,
-  StoreEnhancerStoreCreator,
+  type Action,
+  type Reducer,
+  type StoreEnhancer,
+  type StoreEnhancerStoreCreator,
 } from 'redux';
 import { createContext } from './context';
 import type { Context, StoreConfig } from '@/types';
@@ -31,6 +33,7 @@ const createStore = (props: StoreConfig = {}): Context['store'] => {
       initialState,
       compose<StoreEnhancerStoreCreator<unknown>>(
         ...[
+          mergeInitialState(),
           middlewares ? applyMiddleware(...middlewares) : undefined,
           ...(enhancers || []),
         ].filter(Boolean),
@@ -49,5 +52,37 @@ const createStore = (props: StoreConfig = {}): Context['store'] => {
 
   return store;
 };
+
+/**
+ * Merge prev global state when mounting new models
+ * to avoid to miss the initial state of the mounting models
+ */
+function mergeInitialState(): StoreEnhancer {
+  return createStore => (reducer, initialState) => {
+    const liftReducer = (r: Reducer) => {
+      if (typeof r !== 'function') {
+        throw new Error('Expected the reducer to be a function.');
+      }
+
+      return (state = initialState, action: Action) => {
+        const nextState = r(state, action);
+        if (/^@@redux\/REPLACE/.test(action.type)) {
+          return { ...state, ...nextState };
+        } else {
+          return nextState;
+        }
+      };
+    };
+
+    const store = createStore(liftReducer(reducer));
+
+    return {
+      ...store,
+      replaceReducer: reducer => {
+        return store.replaceReducer(liftReducer(reducer));
+      },
+    };
+  };
+}
 
 export default createStore;
