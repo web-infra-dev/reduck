28d66a23eae2fc97ba339bf5fd7e3ad8f99bbb14
targeral <targeral@outlook.com>
2023-06-08 15:03:05 +0800

    chore: update common/config
    1. change target: es5 -> es2015
    2. add config type definitions

--- common/config.js
+++ common/config.js
@@ -1,8 +1,21 @@
+/**
+ * @typedef {import('@modern-js/module-tools').ModuleUserConfig} ModuleUserConfig
+*/
+
+/**
+ * @type {ModuleUserConfig}
+ */
 module.exports = {
   buildPreset({ preset }) {
     return preset['modern-js-universal'].filter(config => {
       // remove esm code that run in node environment.
       return !config.outDir.includes('esm-node');
+    }).map(config => {
+      // check es5 config, instead of es2015
+      if (config.target === 'es5') {
+        config.target = 'es2015';
+      }
+      return config;
     });
   },
   testing: {
